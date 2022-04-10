ce77c363d54c310ca2eb3bffa8c09651559b40a3
UwU <mmimi0593@gmail.com>
2022-04-10 21:34:18 +0200

    Commentary

--- algo_prim.py
+++ algo_prim.py
@@ -1,102 +1,103 @@
 import random
-import math 
+import math
 
-def matrice_zero (length) :     #Matrice de length par length de 0 
+
+def matrice_zero(length):  # Matrice de length par length de 0
     matrice = []
     for i in range(length):
         matrice.append([])
         for j in range(length):
-            matrice[i].append(0)   
+            matrice[i].append(0)
     return matrice
 
+
 class Graphe:
-    def __init__(self, nombre_sommet) : 
-        self.nombre_sommet = nombre_sommet
-        self.graphe = matrice_zero(nombre_sommet)
-        
-    def add_arete (self, sommet_i, sommet_f, valeur) : 
-        self.graphe [sommet_i] [sommet_f] = valeur
-        self.graphe [sommet_f] [sommet_i] = valeur
+    def __init__(self, nombre_sommet):  # constructeur
+        self.nombre_sommet = nombre_sommet  # défini la variable sommet de l'objet
+        self.graphe = matrice_zero(nombre_sommet)  # créé la matrice du graphe remplie de 0 au taille : nombre_sommet x nombre_sommet
+
+    def add_arete(self, sommet_i, sommet_f, valeur):  # fonction pour ajouter une arète
+        self.graphe[sommet_i][sommet_f] = valeur  # defini la valeur pour aller du 1er sommet au 2eme
+        self.graphe[sommet_f][sommet_i] = valeur  # defini la valeur pour aller du 2eme sommet au 1er
         return self
-    
-    def __str__(self) :
-        affichage = ""
-        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  
-        
-        for j in range (len(self.graphe) + 1):
+
+    def __str__(self):
+        affichage = ""  # valeur a retourner
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  # pour mettre un nom aux cases
+
+        for j in range(len(self.graphe) + 1):  # crée le haut du quadrillage
             if j == 0:
-                affichage  += "┏━"
+                affichage += "┏━"  # début du 1er angle
             else:
-                affichage  += "┳━"
-        affichage += "┓\n┃ ┃"
-        
+                affichage += "┳━"  # séparateur
+        affichage += "┓\n┃ ┃"  # ferme la 1ere ligne du quadruillage et fait un retour a la ligne en ajoutant une case vide
+
+        for i in range(len(self.graphe)):  # pour créer la seconde ligne
+            affichage += alphabet[i] + "┃"  # met les lettres entre des cases
+
+        affichage += "\n"  # retour a la ligne
+
+        for j in range(len(self.graphe) + 1):  # ajoute un séparateur
+            if j == 0:
+                affichage += "┣━"  # début du 1er angle
+            else:
+                affichage += "╋━"  # séparateur
+
+        affichage += "┫\n"  # ferme la dernière case et ajoute une ligne
         for i in range(len(self.graphe)):
-            affichage += alphabet[i] + "┃"
-        
-        affichage += "\n"
-            
-        for j in range (len(self.graphe) + 1):
-            if j == 0:
-                affichage  += "┣━"
-            else:
-                affichage  += "╋━"
-                
-        affichage += "┫\n"
-        for i in range (len(self.graphe)) :
-            affichage += "┃" +  alphabet[i] + "┃"
-            for j in range(len(self.graphe)) :
-                affichage += "\033[3" + str(random.randint(1,6)) + "m" + str(self.graphe [i][j]) + "\033[0m┃"
-            
+            affichage += "┃" + alphabet[i] + "┃"  # met la lettre de la ligne dans une case
+            for j in range(len(self.graphe)):
+                affichage += "\033[3" + str(random.randint(1, 6)) + "m" + str(self.graphe[i][j]) + "\033[0m┃"  # ajoute la valeure avec une couleur aléatoire et ferme la case
+
             affichage += "\n"
-            if i != len(self.graphe) - 1:
-                for j in range (len(self.graphe) + 1):
+            if i != len(self.graphe) - 1:  # vérifie s'il sagit de la dernière ligne
+                for j in range(len(self.graphe) + 1):  # ajoute un séparateur
                     if j == 0:
-                        affichage  += "┣━"
+                        affichage += "┣━"  # début du 1er angle
                     else:
-                        affichage  += "╋━"
-                affichage += "┫\n"
-            else :
-                for j in range (len(self.graphe) + 1):
+                        affichage += "╋━"  # séparateur
+                affichage += "┫\n"  # ferme la dernière case et ajoute une ligne
+            else:
+                for j in range(len(self.graphe) + 1):
                     if j == 0:
-                        affichage  += "┗━"
+                        affichage += "┗━"  # début du 1er angle
                     else:
-                        affichage  += "┻━"
-                affichage += "┛\n"
+                        affichage += "┻━"  # séparateur
+                affichage += "┛\n"  # ferme la dernière case et ajoute une ligne
         return affichage
-    
-    def prim(self) : 
-        
-        grand_nombre = math.inf   #créa nombre infini qu'ils soit plus petit que la valeur du premier noeud
-        
+
+    def prim(self):
+
+        grand_nombre = math.inf  # créa nombre infini qu'ils soit plus petit que la valeur du premier noeud
+
         liste_noeud_parcouru = []
-        for i in range(self.nombre_sommet) :
+        for i in range(self.nombre_sommet):
             liste_noeud_parcouru.append(0)
 
         fin_boucle = 0
 
         liste_noeud_parcouru[0] = True
-        
+
         prim = matrice_zero(self.nombre_sommet)
-        
-        while (fin_boucle < self.nombre_sommet - 1) : 
-            
+
+        while (fin_boucle < self.nombre_sommet - 1):
+
             min = grand_nombre
             sommet_i = 0
             sommet_f = 0
-            
+
             for i in range(self.nombre_sommet):
-                if liste_noeud_parcouru [i] :
-                    for j in range(self.nombre_sommet) :
-                        if (not liste_noeud_parcouru [j] and self.graphe [i] [j]):
-                            if min > self.graphe [i] [j] :
-                                min = self.graphe [i] [j]
-                                sommet_i = i 
-                                sommet_f = j 
-                                
-            liste_noeud_parcouru [sommet_f] = True
-            fin_boucle += 1 
-            prim [sommet_i] [sommet_f] = min 
-            prim [sommet_f] [sommet_i] = prim [sommet_i] [sommet_f]  
-                
-        return prim 
-				
+                if liste_noeud_parcouru[i]:
+                    for j in range(self.nombre_sommet):
+                        if (not liste_noeud_parcouru[j] and self.graphe[i][j]):
+                            if min > self.graphe[i][j]:
+                                min = self.graphe[i][j]
+                                sommet_i = i
+                                sommet_f = j
+
+            liste_noeud_parcouru[sommet_f] = True
+            fin_boucle += 1
+            prim[sommet_i][sommet_f] = min
+            prim[sommet_f][sommet_i] = prim[sommet_i][sommet_f]
+
+        return prim
--- test.py
+++ test.py
@@ -2,19 +2,19 @@ from algo_prim import *
 graph = Graphe(9)
 graph.add_arete(0, 1, 4)
 graph.add_arete(0, 2, 7)
-graph.add_arete(1, 2, 11)
+graph.add_arete(1, 2, 1)
 graph.add_arete(1, 3, 9)
-graph.add_arete(1, 5, 20)
+graph.add_arete(1, 5, 2)
 graph.add_arete(2, 5, 1)
 graph.add_arete(3, 6, 6)
 graph.add_arete(3, 4, 2)
-graph.add_arete(4, 6, 10)
-graph.add_arete(4, 8, 15)
+graph.add_arete(4, 6, 0)
+graph.add_arete(4, 8, 5)
 graph.add_arete(4, 7, 5)
 graph.add_arete(4, 5, 1)
 graph.add_arete(5, 7, 3)
 graph.add_arete(6, 8, 5)
-graph.add_arete(7, 8, 12)
+graph.add_arete(7, 8, 2)
 
 def afficher_matrice (adjacente) :     
     for inner_list in adjacente :
@@ -23,3 +23,4 @@ def afficher_matrice (adjacente) :
         print()
 
 afficher_matrice(graph.prim())
+print(str(graph))
